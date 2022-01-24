01fe1ada2e7c9376b9b34e523ad00bb13b2ca6a6
Alisa Ts <freiheiit92@mail.ru>
2022-01-24 08:55:28 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Alisa Ts on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
